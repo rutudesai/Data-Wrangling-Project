79db8ea54deab12bd39974d022cb597285dce510
rutudesai <rutudesai007@gmail.com>
2022-04-08 12:19:50 -0400

    Updated the Application Script to add a functionality of text searching on YouTube.

--- Script6_04-07-2022_ShinyApp_Project_UI.R
+++ Script6_04-07-2022_ShinyApp_Project_UI.R
@@ -15,12 +15,15 @@ yt_oauth("754004588089-j155ksbuqlnrkgnvrd5l31kh5ugf6qob.apps.googleusercontent.c
 #https://statisticsglobe.com/extract-substring-before-or-after-pattern-in-r
 #to extract pecific pattern after a pattern in r
 ui <- fluidPage(textInput(inputId = "title",
-            label = "Enter the url",
-            value = "https://www.youtube.com/watch?v=YF1eYbfbH5k"),
-      verbatimTextOutput("video"),
-      verbatimTextOutput("stats"),
-      verbatimTextOutput("comments"),
-      verbatimTextOutput("search")
+                          label = "Enter the url",
+                          value = "https://www.youtube.com/watch?v=YF1eYbfbH5k"),
+                verbatimTextOutput("video"),
+                verbatimTextOutput("stats"),
+                verbatimTextOutput("comments"),
+                textInput(inputId = "title_search",
+                          label = "Enter the text to be searched",
+                          value = "Barack Obama"),
+                verbatimTextOutput("search")
 )
 server <- function(input, output){
   video_id <- reactive({
@@ -30,7 +33,7 @@ server <- function(input, output){
   output$video <- renderPrint({
     print("The Video Id is : ") 
     video_id()
-    })
+  })
   
   output$stats <- renderPrint({
     print("Video Statistics are : ")
@@ -41,15 +44,14 @@ server <- function(input, output){
     print("Comments Table : ")
     get_comment_threads(c(video_id=sub(".*v=", "", input$title)), max_results = 101)
   })
-
   
   output$search <- renderPrint({
-    yt_search("Barack Obama") %>% select(video_id,title, channelTitle, description) %>% head(5)
+    print("Searched Results for ",input$title_search," are : ")
+    yt_search(input$title_search) %>% select(video_id,title, channelTitle, description) %>% head(5)
   })
 }
 
 shinyApp(ui = ui, server = server)
 
 #Sharing App
-
 
