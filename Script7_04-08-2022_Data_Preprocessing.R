a2ab6a78232138cffb839c76cb7d60e119bd052d
rutudesai <rutudesai007@gmail.com>
2022-04-09 23:32:59 -0400

    Modified the Script to plot bar chart of Final Sentiment Count.

--- Script7_04-08-2022_Data_Preprocessing.R
+++ Script7_04-08-2022_Data_Preprocessing.R
@@ -19,6 +19,7 @@ ui <- fluidPage(textInput(inputId = "title",
                 verbatimTextOutput("video"),
                 verbatimTextOutput("stats"),
                 verbatimTextOutput("comments"),
+                plotOutput("sentiment_bar"),
                 textInput(inputId = "title_search",
                           label = "Enter the text to be searched",
                           value = "Barack Obama"),
@@ -54,6 +55,23 @@ server <- function(input, output){
       mutate(Sentiment_Category = ifelse(sentiment > 0, "Positive",
                                          ifelse(sentiment < 0, "Negative",
                                                 "Neutral")))
+  })
+  
+  output$sentiment_bar <- renderPlot({
+    get_comment_threads(c(video_id=sub(".*v=", "", input$title)), max_results = 101) %>% 
+      select(videoId, authorDisplayName, textDisplay) %>%
+      mutate(comment_number = row_number()) %>%
+      unnest_tokens(word, textDisplay) %>%
+      anti_join(stop_words)  %>%
+      inner_join(get_sentiments("bing")) %>%
+      mutate(index = comment_number %/% 15) %>%
+      count(index, sentiment) %>%
+      pivot_wider(names_from = "sentiment", values_from = "n") %>%
+      mutate(sentiment = positive - negative) %>%
+      mutate(Sentiment_Category = ifelse(sentiment > 0, "Positive",
+                                         ifelse(sentiment < 0, "Negative",
+                                                "Neutral"))) %>%
+      ggplot(aes(x = Sentiment_Category)) + geom_bar()
   })
   
   output$search <- renderPrint({
