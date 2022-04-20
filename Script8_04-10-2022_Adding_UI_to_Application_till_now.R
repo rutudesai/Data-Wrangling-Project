c6c4ed80c8c14c27b5cb2117bfa5e57c7474a068
rutudesai <rutudesai007@gmail.com>
2022-04-20 14:16:05 -0400

    Made COmments reactive so that code shortens and we get simila output for every graph.

--- Script8_04-10-2022_Adding_UI_to_Application_till_now.R
+++ Script8_04-10-2022_Adding_UI_to_Application_till_now.R
@@ -96,7 +96,7 @@ server <- function(input, output){
     get_stats(video_id())
   })
   
-  output$comments <- renderPrint({
+  comments <- reactive({
     print("Comments Table : ")
     get_comment_threads(c(video_id=sub(".*v=", "", input$title)), max_results = 101) %>% 
       select(videoId, authorDisplayName, textDisplay) %>%
@@ -112,6 +112,11 @@ server <- function(input, output){
       mutate(Sentiment_Category = ifelse(sentiment > 0, "Positive",
                                          ifelse(sentiment < 0, "Negative",
                                                 "Neutral")))
+  })
+  
+  output$comments <- renderPrint({
+    print("The Extracted and Processed Data is : ") 
+    comments()
   })
   
   output$word_cloud_before <- renderPlot({
@@ -140,20 +145,21 @@ server <- function(input, output){
   })
  
   output$overall_sentiment <- renderPlot({
-    get_comment_threads(c(video_id=sub(".*v=", "", input$title)), max_results = 101) %>% 
-      select(videoId, authorDisplayName, textDisplay) %>%
-      mutate(comment_number = row_number()) %>%
-      unnest_tokens(word, textDisplay) %>%
-      anti_join(stop_words)  %>%
-      inner_join(get_sentiments("bing")) %>%
-      mutate(index = comment_number %/% 15) %>%
-      count(index, sentiment) %>%
-      pivot_wider(names_from = "sentiment", values_from = "n") %>%
-      mutate(across(everything(), replace_na, 0)) %>%
-      mutate(sentiment = positive - negative) %>%
-      mutate(Sentiment_Category = ifelse(sentiment > 0, "Positive",
-                                         ifelse(sentiment < 0, "Negative",
-                                                "Neutral"))) %>%
+    #get_comment_threads(c(video_id=sub(".*v=", "", input$title)), max_results = 101) %>% 
+    #  select(videoId, authorDisplayName, textDisplay) %>%
+    #  mutate(comment_number = row_number()) %>%
+    #  unnest_tokens(word, textDisplay) %>%
+    #  anti_join(stop_words)  %>%
+    #  inner_join(get_sentiments("bing")) %>%
+    #  mutate(index = comment_number %/% 15) %>%
+    #  count(index, sentiment) %>%
+    #  pivot_wider(names_from = "sentiment", values_from = "n") %>%
+    #  mutate(across(everything(), replace_na, 0)) %>%
+    #  mutate(sentiment = positive - negative) %>%
+    #  mutate(Sentiment_Category = ifelse(sentiment > 0, "Positive",
+    #                                     ifelse(sentiment < 0, "Negative",
+    #                                            "Neutral"))) %>%
+      comments() %>%
       ggplot(aes(index, sentiment)) + 
       geom_bar(stat="identity") + 
       geom_smooth() +
@@ -163,21 +169,22 @@ server <- function(input, output){
   })
   
   output$sentiment_bar <- renderPlot({
-    get_comment_threads(c(video_id=sub(".*v=", "", input$title)), max_results = 101) %>% 
-      select(videoId, authorDisplayName, textDisplay) %>%
-      mutate(comment_number = row_number()) %>%
-      unnest_tokens(word, textDisplay) %>%
-      anti_join(stop_words)  %>%
-      inner_join(get_sentiments("bing")) %>%
-      mutate(index = comment_number %/% 15) %>%
-      count(index, sentiment) %>%
-      pivot_wider(names_from = "sentiment", values_from = "n") %>%
-      mutate(across(everything(), replace_na, 0)) %>%
-      mutate(sentiment = positive - negative) %>%
-      mutate(Sentiment_Category = ifelse(sentiment > 0, "Positive",
-                                         ifelse(sentiment < 0, "Negative",
-                                                "Neutral"))) %>%
-      ggplot(aes(x = Sentiment_Category)) + geom_bar() + 
+    #get_comment_threads(c(video_id=sub(".*v=", "", input$title)), max_results = 101) %>% 
+    #  select(videoId, authorDisplayName, textDisplay) %>%
+    #  mutate(comment_number = row_number()) %>%
+    #  unnest_tokens(word, textDisplay) %>%
+    #  anti_join(stop_words)  %>%
+    #  inner_join(get_sentiments("bing")) %>%
+    #  mutate(index = comment_number %/% 15) %>%
+    #  count(index, sentiment) %>%
+    #  pivot_wider(names_from = "sentiment", values_from = "n") %>%
+    #  mutate(across(everything(), replace_na, 0)) %>%
+    #  mutate(sentiment = positive - negative) %>%
+    #  mutate(Sentiment_Category = ifelse(sentiment > 0, "Positive",
+    #                                     ifelse(sentiment < 0, "Negative",
+    #                                            "Neutral"))) %>%
+     comments() %>% 
+     ggplot(aes(x = Sentiment_Category)) + geom_bar() + 
       ggtitle("Bar Plot to compare Sentiments in Comments Section") + 
       theme(plot.title = element_text(size = 10, hjust=0.5)) +
       xlab("Sentiment Category") + ylab("Count of Sentiment") + scale_fill_brewer(palette = "Reds")
