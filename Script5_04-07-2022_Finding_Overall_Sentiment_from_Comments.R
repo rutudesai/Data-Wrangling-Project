718c1dab6e6c546d7c008d0a988d8c0a5e14133b
Rutu Desai <43644740+rutudesai@users.noreply.github.com>
2022-10-16 19:53:57 -0400

    Updated Script to remove video id's and text's to be searched
    Updated Script to remove video id's and text's to be searched

--- Script5_04-07-2022_Finding_Overall_Sentiment_from_Comments.R
+++ Script5_04-07-2022_Finding_Overall_Sentiment_from_Comments.R
@@ -17,20 +17,20 @@ library("ggplot2")
 yt_oauth()
 
 # Video Details
-#https://www.youtube.com/watch?v=N708P-A45D0
+#videoURL
 
-#Getting all details about a video with id as "N708P-A45D0"
-get_stats(video_id="N708P-A45D0")
+#Getting all details about a video with id as "videoId"
+get_stats(video_id="videoId")
 
 #Getting Video Details
-get_video_details(video_id="N708P-A45D0")
+get_video_details(video_id="videoId")
 
 #Searching all videos with a specific topic
-res1 <- yt_search("Barack Obama")
+res1 <- yt_search("searchText")
 head(res1[, 1:3])
 
 #Getting comments from a specific video comment section
-res2 <- get_comment_threads(c(video_id="N708P-A45D0"), max_results = 101)
+res2 <- get_comment_threads(c(video_id="videoId"), max_results = 101)
 head(res2)
 
 #Creating df for only specific columns
